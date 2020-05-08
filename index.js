eebe61f391e4846a8a23de55359d32cc2659693c
kareem hamdy <kareemhamdy500@gmail.com>
2020-05-08 02:44:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const config = require('./src/utils/config');
 const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
-    console.log(`[*] server listenning on port ${config.PORT} ...`)
+    console.log(`[*] server listenning on port ${config.PORT} ...`);
 });
